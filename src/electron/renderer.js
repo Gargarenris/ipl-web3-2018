3c3cbac7e2f9c653af2260ef84ef266904470faf
Gargarenris <gargarenris@gmail.com>
2018-11-16 14:47:49 +0100

    electron and webpack

---
+++ src/electron/renderer.js
@@ -0,0 +1,6 @@
+const scripts = [];
+// Dynamically insert the DLL script in development env in the
+// renderer process
+// Dynamically insert the bundled app script in the renderer process
+scripts.push('http://localhost:3000/application.js');
+document.write(scripts.map(script => `<script defer src="${script}"><\/script>`).join(''));
