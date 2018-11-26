0cfe54bbe79c2167511e751c12362b51002825fb
SPEICHER  Arthur <arthur.speicher@student.vinci.be>
2018-11-26 18:42:34 +0100

    menus + task

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,24 +1,70 @@
-const {app, BrowserWindow, Tray, Menu } = require('electron');
+const {app, BrowserWindow, Tray, Menu, shell } = require('electron');
 const path = require('path');
+
+//process.env.NODE_ENV='dev'; //process.env.NODE_ENV='production'; //permet de modifier le menu 
+
 
 let mainWindow;
 const MAC_PLATFORM = "darwin"; // apparament mac = darwin 
 
 const mainMenuTemplate = [ // a modifier avec le menu d'arthur
     {
-        label : 'File',
+        label : 'Options',
         submenu:[
             {
-                label : 'Quit',
+                label : 'Theme',
+                accelerator : process.platform == 'darwin' ? 'Command+T' : 'Ctrl+T',
+                click(){
+                    mainWindow.webContents.send('ping', 'whooooh');
+                }
+            },
+            {
+                type:'separator'
+            },
+            {
+                label:'Quitter',
                 accelerator : process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
                 click(){
-                    //app.quit();
-                    mainWindow.webContents.send('ping', 'whooooh');
+                    app.quit();
                 }
             }
         ]
     }
 ]
+
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push(
+        {
+            label:'Dev Tools',
+            accelerator : process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+            click(){
+                if(BrowserWindow.getFocusedWindow()==null) return;
+                BrowserWindow.getFocusedWindow().toggleDevTools();
+            }
+        },
+        {
+            label:'Help',
+            submenu: [{
+                label:'Documentation electron',
+                accelerator : process.platform == 'darwin' ? 'Command+H' : 'Ctrl+H',
+                click(){
+                    shell.openExternal('https://electronjs.org/docs');
+                }
+            }]
+        }
+    )
+}
+
+app.setUserTasks([
+    {
+      program: process.execPath,
+      arguments: '',
+      iconPath: process.execPath,
+      iconIndex: 0,
+      title: 'Nouvelle fenêtre',
+      description: 'Créer une nouvelle fenêtre'
+    }
+  ])
 
 function createWindow(){
 
@@ -30,6 +76,7 @@ function createWindow(){
         mainWindow = null;
     });
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
     tray.setContextMenu(mainMenu);
 }
 
